aec619d3c2889d1e79df41487b73010143670b93
Tim Holzhey <tholzhey.th@gmail.com>
2022-07-19 00:29:00 +0200

    Added simple application example

--- main.c
+++ main.c
@@ -3,11 +3,28 @@
 //
 
 #include "test_json.h"
+#include "json.h"
+#include <stdio.h>
+
+#define RUN_TESTS	1
 
 int main() {
+#if defined RUN_TESTS && RUN_TESTS == 1
 	test_json_lex();
 	test_json_parse();
 	test_json_build();
 	test_json_stringify();
+#else
+	const char *str = "{\"key\":\"value\"}";
+	json_parse_string(str, obj);
+
+	printf("Parsing input: %s\n\n", str);
+	printf("Parser returned: %d\n", obj_return);
+	printf("Has key 'key': %d\n", json_object_has_key(&obj, "key"));
+	printf("Value is: %s\n", json_object_get_value(&obj, "key")->string);
+	printf("Value type: %u\n", json_object_get_value_type(&obj, "key"));
+	printf("Minimal print: %s\n", json_stringify(&obj));
+	printf("Pretty print: %s\n", json_stringify_pretty(&obj));
+#endif
     return 0;
 }
